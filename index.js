d19efe30b440d20d85369c5b19ac76adbe9ba698
Mateo Leguizamon <matu_legui_22@hotmail.com>
2022-01-17 18:31:35 -0300

    Agregue Scripts - Cree un principio de tabla de multiplicar

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('prueba 2');
+const fs = require('fs');
+
+const base = 2;
+
+console.log(`================== 
+    Tabla del ${base}
+==================`);
+let resultado = ' ';
+for (let i = 1; i <= 10; i++) {
+    resultado += `${base} * ${i} = ${base * i}\n`
+}
+
+console.log(resultado);
+
+fs.writeFileSync(`tablas/tabla-del-${base}`, resultado)
