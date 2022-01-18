65f21582c07ccbe5da843ada8db2278ee0e87303
Mateo Leguizamon <matu_legui_22@hotmail.com>
2022-01-18 06:10:11 -0300

    Se agreo la dependencia yarn, se agrego la dependecia npm color, se termino la app

--- multiplicador/index-mult.js
+++ multiplicador/index-mult.js
@@ -1,19 +1,24 @@
+const colors = require ('colors');
 const fs = require('fs');
 const {logica} = require('./logica');
 
-const multiplicar = (base) => {
-    console.log(`================== 
-    Tabla del ${base}
-==================`);
-    const resultado = logica(base);    
 
-    console.log(resultado);
-    const nombreArchivo = `tabla-del-${base}`
+const multiplicar = (base, limite, visualizar) => {
+    
+    const resultado = logica(base, limite);    
+    
+    if(visualizar){
+        console.log('============='.rainbow);
+        console.log(`tabla del ${base}`.america);
+        console.log('============='.zebra);
+        console.log(resultado);
+    }
+    
+    const nombreArchivo = `tabla-del-${base}.txt`
     fs.writeFileSync(`tablas/${nombreArchivo}`, resultado, (err) => {
         if (err) throw err;
-        console.log(`El archivo ${nombreArchivo} fue creado con Exito!`);
     });
-
+    console.log(`El archivo ${nombreArchivo} fue creado con Exito!`.random);
 }
 
 module.exports = {
